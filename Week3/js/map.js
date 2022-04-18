05bda321363118b55777c5e1c4497850853e97c1
Audrey Tey <audreytey@g.ucla.edu>
2022-04-17 18:58:01 -0700

    Week 3 Assignment

--- Week3/js/map.js
+++ Week3/js/map.js
@@ -1,53 +1,84 @@
-// let's create some data
-let data = [
-    {
-        'id': 0,
-        'title':'Osaka',
-        'lat': 34.6937,
-        'lon': 135.5023,
-        'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Osaka_Castle_02bs3200.jpg/320px-Osaka_Castle_02bs3200.jpg',
-        'description': "Osaka is a large port city and commercial center on the Japanese island of Honshu. It's known for its modern architecture, nightlife and hearty street food. The 16th-century shogunate Osaka Castle, which has undergone several restorations, is its main historical landmark. It's surrounded by a moat and park with plum, peach and cherry-blossom trees. Sumiyoshi-taisha is among Japan’s oldest Shinto shrines. ― Google"
-
-    },
-    {
-        'id': 1,
-        'title':'Cali',
-        'lat': 3.4516,
-        'lon': -76.5320,
-        'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Pascual_Guerrero_U-20WC_2011_CMR-NZL.JPG/320px-Pascual_Guerrero_U-20WC_2011_CMR-NZL.JPG',
-        'description':"Cali is a Colombian city in the Valle del Cauca department, southwest of Bogotá. It’s known for salsa dancing and there are many clubs in the suburb of Juanchito. In Cali’s oldest quarter, the neoclassical San Pedro Cathedral houses paintings of the Quito School. Nearby is the 18th- and 19th-century San Francisco Religious Complex. La Merced Chapel is where the first mass was held after Cali was founded in 1536. ― Google"
-    },
-    {
-        'id': 2,
-        'title':'Bangkok',
-        'lat': 13.7563,
-        'lon': 100.5018,
-        'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Bangkok_skytrain_sunset.jpg/320px-Bangkok_skytrain_sunset.jpg',
-        'description':"Bangkok, Thailand’s capital, is a large city known for ornate shrines and vibrant street life. The boat-filled Chao Phraya River feeds its network of canals, flowing past the Rattanakosin royal district, home to opulent Grand Palace and its sacred Wat Phra Kaew Temple. Nearby is Wat Pho Temple with an enormous reclining Buddha and, on the opposite shore, Wat Arun Temple with its steep steps and Khmer-style spire. ― Google"
-    },
-    {
-        'id': 3,
-        'title':'Tokyo',
-        'lat': 35.6762,
-        'lon': 139.6503,
-        'image':'https://www.stofficetokyo.ch/sites/default/files/styles/featured_image_840x572_/public/2019-01/icu_dronei_34ab_170430-42_r.jpg?itok=mkG94UWD',
-        'description':"Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) to a reconstructed kabuki theater (in the Edo-Tokyo Museum). ― Google"
-    },
-    {
-        'id': 4,
-        'title':'Los Angeles',
-        'lat': 34.0522,
-        'lon': -118.2437,
-        'image':'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/UCLA_Royce_Hall.jpg/320px-UCLA_Royce_Hall.jpg',
-        'description':"Los Angeles is a sprawling Southern California city and the center of the nation’s film and television industry. Near its iconic Hollywood sign, studios such as Paramount Pictures, Universal and Warner Brothers offer behind-the-scenes tours. On Hollywood Boulevard, TCL Chinese Theatre displays celebrities’ hand- and footprints, the Walk of Fame honors thousands of luminaries and vendors sell maps to stars’ homes. ― Google"
-    },
-]
-
-let map = L.map('map').setView([0,0], 3);
+// map settings
+let map = L.map('map').setView([2.5, 115.6628], 4.25);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// locations
+let data = [
+    {
+        'id': 0,
+        'title':'Indonesia',
+        'description':'Indonesia is the largest country in ASEAN, with a population of 270 million, it is the world\'s fourth-most populous country. It is also currently the G20 leader.',
+        'lat': -6.12,
+        'lon': 106.4
+    },
+    {
+        'id': 1,
+        'title':'Philippines',
+        'description':'Philippines is an archipelagic country (island country) with diverse ethnicities and cultures throughout its islands.',
+        'lat': 14.38,
+        'lon': 121.02
+    },
+    {
+        'id': 2,
+        'title':'Thailand',
+        'description':'Thailand, also known as Siam or the Kingdom of Thailand, is a constitutional monarchy with the King serves as the head of state and is highly respected by the people, although his powers are limited by the constitution and he is primarily a symbolic figurehead. From 2014 to 2019, Thailand was formally under military rule, until the military introduced a new constitution and held elections which established the framework of a parliamentary constitutional monarchy.',
+        'lat': 15.8700,
+        'lon': 100.9925
+    },
+    {
+        'id': 3,
+        'title':'Singapore',
+        'description':'Singapore is an island city-state with the third greatest population density in the world. After early years of turbulence and despite lacking natural resources and a hinterland, the nation rapidly developed to become one of the Four Asian Tigers based on external trade. Today, it is a major financial and shipping hub and has consistently ranked highly in key social indicators such as education, healthcare and quality of life.',
+        'lat': 1.3521,
+        'lon': 103.8198
+    },
+    {
+        'id': 4,
+        'title':'Vietnam',
+        'description':'Vietnam, officially the Socialist Republic of Vietnam, was reunified as a unitary socialist state under the Communist Party of Vietnam in 1976 following the end of the Vietnam War. In 1986, the Communist Party initiated economic and political reforms, transforming the country to a market-oriented economy.',
+        'lat': 14.0583,
+        'lon': 108.2772
+    },
+    {
+        'id': 5,
+        'title':'Malaysia',
+        'description':'Malaysia consists of thirteen states and three federal territories. Kuala Lumpur is the national capital of Malaysia, Putrajaya is the administrative capital, and Labuan is an offshore international financial centre.',
+        'lat': 3.1390,
+        'lon': 101.6869
+    },
+    {
+        'id': 6,
+        'title':'Brunei',
+        'description':'Brunei is located on the north coast of the island of Borneo. Crude oil and natural gas production account for about 90% of its GDP.',
+        'lat': 4.5353,
+        'lon': 114.7277
+    },
+    {
+        'id': 7,
+        'title':'Cambodia',
+        'description':'Cambodia borders Thailand, Laos, and Vietnam. Buddhism is enshrined in the constitution as the official state religion, and is practised by more than 97% of the population.',
+        'lat': 12.5657,
+        'lon': 104.9910
+    },
+    {
+        'id': 8,
+        'title':'Laos',
+        'description':'Laos is the only landlocked country in Southeast Asia and has a one-party socialist government.',
+        'lat': 19.8563,
+        'lon': 102.4955
+    },
+    {
+        'id': 9,
+        'title':'Myanmar',
+        'description':'Myanmar is the largest country in mainland Southeast Asia, and has a population of about 54 million. In February 2021, the military junta launched a coup d\'état and overthrew the democratically-elected ruling party after they won by a landslide. Since then, the military has utilized violence to put down opposition activists and civilians, leading to a severe humanitarian crisis in Myanmar today.',
+        'lat': 21.9162,
+        'lon': 95.9560
+    },
+]
+
 
 // before looping the data, create an empty FeatureGroup
 let myMarkers = L.featureGroup();
@@ -55,13 +86,22 @@ let myMarkers = L.featureGroup();
 // loop through data
 data.forEach(function(item){
 	// create marker
-	let marker = L.marker([item.lat,item.lon]).bindPopup(item.title)
+	let marker = L.marker([item.lat,item.lon], 
+        // map marker icons
+        {icon: new L.Icon({
+            iconUrl: `./countryflags/${item.title}.png`,
+            iconSize: [50, 30],
+            iconAnchor: [10, 0],
+        })})
+    .bindPopup('<b>'+item.title+'</b>' + '<br>' + item.description)
 
 	// add marker to featuregroup
 	myMarkers.addLayer(marker)
 
 	// add data to sidebar with onclick event
-	$('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${item.id})">${item.title}</div>`)
+	$('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${item.id})">
+    <img class="sidebar-item" src="./countryflags/${item.title}.png" width="55px">
+    </div>`)
 })
 
 // after loop, add the FeatureGroup to map
