6f2db4c8b1f05bd08962b99fa93aab5d499893ea
Audrey Tey <audreytey@g.ucla.edu>
2022-04-18 15:45:30 -0700

    Week 4 Lab

--- Week4/js/map.js
+++ Week4/js/map.js
@@ -12,58 +12,59 @@ let markers = L.featureGroup();
 
 // initialize
 $( document ).ready(function() {
-	createMap(lat,lon,zl);
+    createMap(lat,lon,zl);
     readCSV(path);
 });
 
 
 // create the map
 function createMap(lat,lon,zl){
-	map = L.map('map').setView([lat,lon], zl);
+    map = L.map('map').setView([lat,lon], zl);
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 }
 
 // function to read csv data
 function readCSV(path){
-	Papa.parse(path, {
-		header: true,
-		download: true,
-		complete: function(data) {
-			console.log(data);
-			
-			// map the data
-			mapCSV(data);
+    Papa.parse(path, {
+        header: true,
+        download: true,
+        complete: function(data) {
+            console.log(data);
+            
+            // map the data
+            mapCSV(data);
 
-		}
-	});
+        }
+    });
 }
 
 // mapCSV() function: that takes in the data from the csv file, creates a marker for each element, and maps it
-// this is the original non-circle marker 
+
+// this is the original function with the non-circle marker, no hover event, no sidebar images, no panTo function 
+/* function mapCSV(data){
+    
+    // loop through each entry
+    data.data.forEach(function(item,index){
+        // create marker
+        let marker = L.marker([item.latitude,item.longitude])
+
+        // add marker to featuregroup
+        markers.addLayer(marker)
+    })
+
+    // add featuregroup to map
+    markers.addTo(map)
+
+    // fit markers to map
+    map.fitBounds(markers.getBounds())
+} */
+
+
 function mapCSV(data){
-	
-	// loop through each entry
-	data.data.forEach(function(item,index){
-		// create marker
-		let marker = L.marker([item.latitude,item.longitude])
 
-		// add marker to featuregroup
-		markers.addLayer(marker)
-	})
-
-	// add featuregroup to map
-	markers.addTo(map)
-
-	// fit markers to map
-	map.fitBounds(markers.getBounds())
-}
-
-/*
-function mapCSV(data){
-	
 	// circle options
 	let circleOptions = {
 		radius: 5,
@@ -75,16 +76,27 @@ function mapCSV(data){
 
 	// loop through each entry
 	data.data.forEach(function(item,index){
-		// create marker
+		// create a marker
 		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+		.on('mouseover',function(){
+			this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
+		})
 
-		// add marker to featuregroup		
+		// add marker to featuregroup
 		markers.addLayer(marker)
+
+		// add entry to sidebar
+		$('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
 	})
 
 	// add featuregroup to map
 	markers.addTo(map)
 
-	// fit markers to map
+	// fit map to markers
 	map.fitBounds(markers.getBounds())
+}
+
+function panToImage(index){
+	map.setZoom(17);
+	map.panTo(markers.getLayers()[index]._latlng);
 }
