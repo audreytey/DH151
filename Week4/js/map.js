2573ad92d6c336fa543d8034bed9904774c573b7
Audrey Tey <audreytey@g.ucla.edu>
2022-04-24 23:47:44 -0700

    Week 4 assignment

--- Week4/js/map.js
+++ Week4/js/map.js
@@ -20,9 +20,9 @@ $( document ).ready(function() {
 function createMap(lat,lon,zl){
 	map = L.map('map').setView([lat,lon], zl);
 
-	L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+	L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+	}).addTo(map); // CartoDB.Voyager base map layer
 }
 
 function flyToIndex(lat, lon){
